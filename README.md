2195ace17e2d28335b1ce1d952170914e7d397b7
Divya Jain <divyavivekjain@gmail.com>
2017-09-26 08:03:21 -0400

    rename to index

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Train-Scheduler
-Train schedule application
+Train schedule application:  
+ train schedule application that incorporates Firebase to host arrival and departure data. App will retrieve and manipulate this information with Moment.js. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+
+Technologies used
+
+Google Fonts
+jQuery
+Firebase
+Prerequisites
+
+
+- jQuery: visit http://code.jquery.com/ and link in html
+- Firebase: https://firebase.google.com/
+Built With
+
+Sublime Text - Text Editor
+
+Authors
+
+Stefanie Ding - HTML/CSS/JS - Stefanie Ding
+
+
+Created during Week 7 of Rutgers Coding Bootcamp. Firebase Application to accept input from user to create a fictional train list and calculate the next train and minutes away.
+
+Author
+
+Divya Jain
--- assets/js/train.js
+++ assets/js/train.js
@@ -1,4 +1,4 @@
-  // Initialize Firebase
+   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBDIaD1hC-jZbgXbnaJ7ibV0mve2Y69ZnI",
     authDomain: "my-train-scheduler.firebaseapp.com",
@@ -7,16 +7,30 @@
     storageBucket: "my-train-scheduler.appspot.com",
     messagingSenderId: "73989011162"
   };
+
   firebase.initializeApp(config);
+
   var database = firebase.database();
-	
+	var trainName ="";
+  var destination = "";
+  var arrival = 0;
+  var frequency = 0;
+  var trainData = "";
+  var destinationData = "";
+  var arrivalData = 0;
+  var frequencyData = 0;
+  var arrivalData = 0;
+  var nextTrain = 0;
+  var  nextTrainFormatted = 0 ;
+
   // add on click funtion to add values
 	$("#addUser").on("click",function() {
 	 event.preventDefault();
-	 var trainName = $("#name-input").val().trim();
-	 var destination = $("#destination-input").val().trim();
-	 var arrival = $("#traintime-input").val().trim();
-	 var frequency = $("#frequency-input").val().trim();
+   
+	 trainName = $("#name-input").val().trim();
+	 destination = $("#destination-input").val().trim();
+	 arrival = $("#traintime-input").val().trim();
+	 frequency = $("#frequency-input").val().trim();
    
 	 database.ref().push({
  	 trainName : trainName,
@@ -24,28 +38,27 @@
  	 arrival : arrival,
    frequency : frequency
    
-  });
+   });
+   });
 
 	database.ref().on("child_added", function(childSnapshot, prevChildKey){
 	console.log(childSnapshot.val());
-	var trainData = childSnapshot.val().trainName;
-	var destinationData = childSnapshot.val().destination;
-	var frequencyData = childSnapshot.val().frequency;
-	var arrivalData = childSnapshot.val().arrival;	
- 	console.log(trainData);
-	console.log(destinationData);
-	console.log(frequencyData);
-	console.log(arrivalData);
+	 trainData = childSnapshot.val().trainName;
+	 destinationData = childSnapshot.val().destination;
+	 frequencyData = childSnapshot.val().frequency;
+	 arrivalData = childSnapshot.val().arrival;	
+ 	
   // moment.js time converted and next arrival time calculated
   firstTimeConverted = moment(arrival, "hh:mm").subtract(1, "years");
   currentTime = moment();
   diffTime = moment().diff(moment(firstTimeConverted), "minutes");
   tRemainder = diffTime % frequency;
   minutesTillTrain = frequency - tRemainder;
- var nextTrain = moment().add(minutesTillTrain, "minutes");
- var  nextTrainFormatted = moment(nextTrain).format("hh:mm");
+  nextTrain = moment().add(minutesTillTrain, "minutes");
+  nextTrainFormatted = moment(nextTrain).format("hh:mm");
   // appending the values to the browser
  $("#table > tbody").append("<tr><td>" + trainData + "</td><td>" + destinationData + "</td><td>" +
    arrivalData + "</td><td>" + frequencyData + "</td><td>"  + nextTrainFormatted +  "</td></tr>");
+  }, function (errorObject){
+    console.log(errorObject.code);
   });
-})
